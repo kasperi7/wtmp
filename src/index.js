4dcb01aff0234a4862b6c032dc24443915b9c075
Kasperi <kasperih@metropolia.fi>
2022-01-30 22:18:57 +0200

    task1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log('Hello console!');
+const cheat = (password) => {
+  let keylogger = [];
+  document.addEventListener("keyup", (event) => {
+    keylogger.push(event.key);
+    if (keylogger.join("") === password) {
+      keylogger = [];
+      alert("cheated");
+    }
+  });
+};
+cheat("cheat");
+
+document.querySelector(".touchElement").addEventListener("mouseover", () => {
+  console.log("touches");
+});
+
+const timer = setTimeout(() => {
+  document.querySelector("#hurryUp").textContent = "Hurry up";
+}, 15000);
+
+const idleTimer = () => {
+  let timeout;
+  document.addEventListener("mousemove", () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      document.querySelector("#idle").textContent = "You should hurry up!";
+    }, 15000);
+    document.querySelector("#idle").textContent = " ";
+  });
+};
+idleTimer();
+
+const clicks = () => {
+  document.addEventListener("dblclick", (event) => {
+    let x = event.clientX;
+    let y = event.clientY;
+    let coordinates = "x: " + x + " | y: " + y;
+    console.log(coordinates);
+  });
+};
+clicks();
